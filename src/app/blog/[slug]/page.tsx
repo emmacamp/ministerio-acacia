6391810c0b6ceeb11b02fb96791271b96326abb8
Emmanuel Popa <emmanuelaaronpopa@gmail.com>
2024-09-11 14:28:18 -0400

    blog content

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -12,27 +12,18 @@ const BlogDetails = () => {
     <div className='flex w-full flex-col items-center bg-white-a700'>
       <div className='container-xs md:px-5'>
         <div className='flex flex-col items-center bg-white-a700 px-14 md:px-5'>
-          <div className='mb-8 flex w-[72%] flex-col gap-7 md:w-full'>
+          <div className='mb-8 flex w-[72%] mt-5 flex-col gap-7 md:w-full'>
             <div className='flex flex-col gap-8'>
               <div className='flex flex-col gap-5'>
                 <div className='flex flex-col items-start gap-4'>
-                  <Button
-                    data-aos='fade-up'
-                    className='flex h-[32px] min-w-[92px] flex-row items-center justify-center rounded-lg bg-light_blue-700 px-3 text-center font-worksans text-[14px] font-medium text-white-a700'
-                  >
+                  <Button className='flex h-[32px] min-w-[92px] flex-row items-center justify-center rounded-lg bg-light_blue-700 px-3 text-center font-worksans text-[14px] font-medium text-white-a700'>
                     {blog?.buttonLabel}
                   </Button>
-                  <Heading
-                    data-aos='fade-up'
-                    size='heading2'
-                    color='dark'
-                    as='h1'
-                    className='w-full leading-10'
-                  >
+                  <Heading size='heading2' color='dark' as='h1' className='w-full leading-10'>
                     {blog?.titleText}
                   </Heading>
                 </div>
-                <div data-aos='fade-up' className='flex'>
+                <div className='flex'>
                   <Text size='textxs' as='p' className='self-end !font-worksans !text-gray-600'>
                     {blog?.dateText}
                   </Text>
@@ -40,7 +31,6 @@ const BlogDetails = () => {
               </div>
               <div className='flex flex-col gap-[30px]'>
                 <Img
-                  data-aos='fade-up'
                   src={blog?.userImage}
                   width={800}
                   height={462}
@@ -48,7 +38,6 @@ const BlogDetails = () => {
                   className='h-[462px] rounded-[24px] object-cover'
                 />
                 <Text
-                  data-aos='fade-up'
                   size='textlg'
                   as='div'
                   className='!font-sourceserifpro leading-8 !text-gray-800'
@@ -61,11 +50,10 @@ const BlogDetails = () => {
 
                 {blog?.content.subjects.map((subject, i) => (
                   <div key={`subject-${i}`} className='flex flex-col items-start gap-3.5'>
-                    <Heading data-aos='fade-up' color='dark' as='h2'>
+                    <Heading color='dark' as='h2'>
                       {subject.title}
                     </Heading>
                     <Text
-                      data-aos='fade-up'
                       size='textlg'
                       as='div'
                       className='w-full !font-sourceserifpro leading-8 !text-gray-800'
--- src/sections/home/InstagramFeed.tsx
+++ src/sections/home/InstagramFeed.tsx
@@ -3,6 +3,7 @@ import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import './instagram.css';
 import { Post } from './RedesSection';
+import Image from 'next/image';
 
 interface FeedProps {
   className: string;
@@ -15,44 +16,114 @@ export const InstagramFeedDesktop = ({ posts, className }: FeedProps) => {
       <div className={`self-stretch ${className}`}>
         <div className='flex gap-7 phone-md:flex-col phone-md:justify-center phone-md:items-center'>
           <div className='flex flex-1 flex-col gap-7 phone-md:w-full'>
-            <img
-              src={posts[1].media_url}
-              width={284}
-              height={284}
-              alt='Imagetwentytwo'
-              className='h-[284px] rounded-[24px] object-cover phone-md:w-full'
-            />
-            <img
-              src={posts[2].media_url}
-              width={284}
-              height={284}
-              alt='Imagetwentyfour'
-              className='h-[284px] rounded-[24px] object-cover phone-md:w-full'
-            />
+            {posts[1].media_type === 'VIDEO' ? (
+              <video
+                src={posts[1].media_url}
+                autoPlay
+                loop
+                muted
+                playsInline
+                // controls
+                width={284}
+                height={284}
+                className='h-[284px] rounded-[24px] object-cover phone-md:w-full'
+              />
+            ) : (
+              <Image
+                src={posts[1].media_url}
+                width={284}
+                height={284}
+                alt='Instagram Feed'
+                className='h-[284px] rounded-[24px] object-cover phone-md:w-full'
+              />
+            )}
+            {posts[2].media_type === 'VIDEO' ? (
+              <video
+                autoPlay
+                muted
+                loop
+                playsInline
+                src={posts[2].media_url}
+                width={284}
+                height={284}
+                className='h-[284px] rounded-[24px] object-cover phone-md:w-full'
+                // controls
+              />
+            ) : (
+              <Image
+                src={posts[2].media_url}
+                width={284}
+                height={284}
+                alt='Instagram Feed'
+                className='h-[284px] rounded-[24px] object-cover phone-md:w-full'
+              />
+            )}
           </div>
-          <img
-            src={posts[0].media_url}
-            width={596}
-            height={596}
-            alt='Imagetwentyone'
-            className='phone-md:justify-center phone-md:w-full phone-md:items-center tablet:w-[300px] phone-md:h-[284px] h-[596px] w-[596px] rounded-[24px] object-cover self-stretch'
-          />
+          {posts[0].media_type === 'VIDEO' ? (
+            <video
+              autoPlay
+              muted
+              loop
+              playsInline
+              src={posts[0].media_url}
+              width={596}
+              height={596}
+              className='phone-md:w-full phone-md:h-[284px] h-[596px] w-[596px] rounded-[24px] object-cover self-stretch'
+              // controls
+            />
+          ) : (
+            <Image
+              src={posts[0].media_url}
+              width={596}
+              height={596}
+              alt='Instagram Feed'
+              className='phone-md:w-full phone-md:h-[284px] h-[596px] w-[596px] rounded-[24px] object-cover self-stretch'
+            />
+          )}
 
           <div className='flex flex-1 flex-col gap-7 self-stretch'>
-            <img
-              src={posts[3].media_url}
-              width={284}
-              height={284}
-              alt='Imagetwentythre'
-              className='h-[284px] phone-md:w-full w-[284px] rounded-[24px] object-cover'
-            />
-            <img
-              src={posts[4].media_url}
-              width={284}
-              height={284}
-              alt='Imagetwentyone'
-              className='h-[284px] phone-md:w-full w-[284px] rounded-[24px] object-cover'
-            />
+            {posts[3].media_type === 'VIDEO' ? (
+              <video
+                autoPlay
+                muted
+                loop
+                playsInline
+                src={posts[3].media_url}
+                width={284}
+                height={284}
+                className='h-[284px] phone-md:w-full w-[284px] rounded-[24px] object-cover'
+                // controls
+              />
+            ) : (
+              <Image
+                src={posts[3].media_url}
+                width={284}
+                height={284}
+                alt='Instagram Feed'
+                className='h-[284px] phone-md:w-full w-[284px] rounded-[24px] object-cover'
+              />
+            )}
+            {posts[4].media_type === 'VIDEO' ? (
+              <video
+                autoPlay
+                muted
+                playsInline
+                loop
+                src={posts[4].media_url}
+                width={284}
+                height={284}
+                className='h-[284px] phone-md:w-full w-[284px] rounded-[24px] object-cover'
+                // controls
+              />
+            ) : (
+              <Image
+                src={posts[4].media_url}
+                width={284}
+                height={284}
+                alt='Instagram Feed'
+                className='h-[284px] phone-md:w-full w-[284px] rounded-[24px] object-cover'
+              />
+            )}
           </div>
         </div>
       </div>
@@ -61,7 +132,8 @@ export const InstagramFeedDesktop = ({ posts, className }: FeedProps) => {
 };
 
 export const InstagramFeedMobile = ({ posts, className }: FeedProps) => {
-  const handleDragStart = (e: React.DragEvent<HTMLImageElement>) => e.preventDefault();
+  const handleDragStart = (e: React.DragEvent<HTMLImageElement | HTMLVideoElement>) =>
+    e.preventDefault();
 
   const responsive = {
     0: { items: 1 },
@@ -69,43 +141,33 @@ export const InstagramFeedMobile = ({ posts, className }: FeedProps) => {
     1024: { items: 3 },
   };
 
-  const items = [
-    <img
-      key={posts[0].id}
-      src={posts[0].media_url}
-      onDragStart={handleDragStart}
-      className='item'
-      role='presentation'
-    />,
-    <img
-      key={posts[1].id}
-      src={posts[1].media_url}
-      onDragStart={handleDragStart}
-      className='item'
-      role='presentation'
-    />,
-    <img
-      key={posts[2].id}
-      src={posts[2].media_url}
-      onDragStart={handleDragStart}
-      className='item'
-      role='presentation'
-    />,
-    <img
-      key={posts[3].id}
-      src={posts[3].media_url}
-      onDragStart={handleDragStart}
-      className='item'
-      role='presentation'
-    />,
-    <img
-      key={posts[4].id}
-      src={posts[4].media_url}
-      onDragStart={handleDragStart}
-      className='item'
-      role='presentation'
-    />,
-  ];
+  const items = posts.map((post) =>
+    post.media_type === 'VIDEO' ? (
+      <video
+        key={post.id}
+        src={post.media_url}
+        autoPlay
+        muted
+        loop
+        playsInline
+        onDragStart={handleDragStart}
+        className='item'
+        role='presentation'
+        // controls
+      />
+    ) : (
+      <Image
+        key={post.id}
+        src={post.media_url}
+        width={800}
+        height={500}
+        alt='Instagram Feed'
+        onDragStart={handleDragStart}
+        className='item !object-cover'
+        role='presentation'
+      />
+    )
+  );
 
   return (
     <div className={`h-[auto] rounded-md overflow-hidden ${className}`}>
@@ -115,9 +177,120 @@ export const InstagramFeedMobile = ({ posts, className }: FeedProps) => {
         autoPlayInterval={2000}
         responsive={responsive}
         items={items}
-
-        // controlsStrategy='alternate'
       />
     </div>
   );
 };
+
+// export const InstagramFeedDesktop = ({ posts, className }: FeedProps) => {
+//   return (
+//     posts?.length > 0 && (
+//       <div className={`self-stretch ${className}`}>
+//         <div className='flex gap-7 phone-md:flex-col phone-md:justify-center phone-md:items-center'>
+//           <div className='flex flex-1 flex-col gap-7 phone-md:w-full'>
+//             < Image
+//               src={posts[1].media_url}
+//               width={284}
+//               height={284}
+//                alt='Instagram Feed''Imagetwentytwo'
+//               className='h-[284px] rounded-[24px] object-cover phone-md:w-full'
+//             />
+//             < Image
+//               src={posts[2].media_url}
+//               width={284}
+//               height={284}
+//                alt='Instagram Feed''Imagetwentyfour'
+//               className='h-[284px] rounded-[24px] object-cover phone-md:w-full'
+//             />
+//           </div>
+//           < Image
+//             src={posts[0].media_url}
+//             width={596}
+//             height={596}
+//              alt='Instagram Feed''Imagetwentyone'
+//             className='phone-md:justify-center phone-md:w-full phone-md:items-center tablet:w-[300px] phone-md:h-[284px] h-[596px] w-[596px] rounded-[24px] object-cover self-stretch'
+//           />
+
+//           <div className='flex flex-1 flex-col gap-7 self-stretch'>
+//             < Image
+//               src={posts[3].media_url}
+//               width={284}
+//               height={284}
+//                alt='Instagram Feed''Imagetwentythre'
+//               className='h-[284px] phone-md:w-full w-[284px] rounded-[24px] object-cover'
+//             />
+//             < Image
+//               src={posts[4].media_url}
+//               width={284}
+//               height={284}
+//                alt='Instagram Feed''Imagetwentyone'
+//               className='h-[284px] phone-md:w-full w-[284px] rounded-[24px] object-cover'
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     )
+//   );
+// };
+
+// export const InstagramFeedMobile = ({ posts, className }: FeedProps) => {
+//   const handleDragStart = (e: React.DragEvent<HTMLImageElement>) => e.preventDefault();
+
+//   const responsive = {
+//     0: { items: 1 },
+//     568: { items: 2 },
+//     1024: { items: 3 },
+//   };
+
+//   const items = [
+//     < Image
+//       key={posts[0].id}
+//       src={posts[0].media_url}
+//       onDragStart={handleDragStart}
+//       className='item'
+//       role='presentation'
+//     />,
+//     < Image
+//       key={posts[1].id}
+//       src={posts[1].media_url}
+//       onDragStart={handleDragStart}
+//       className='item'
+//       role='presentation'
+//     />,
+//     < Image
+//       key={posts[2].id}
+//       src={posts[2].media_url}
+//       onDragStart={handleDragStart}
+//       className='item'
+//       role='presentation'
+//     />,
+//     < Image
+//       key={posts[3].id}
+//       src={posts[3].media_url}
+//       onDragStart={handleDragStart}
+//       className='item'
+//       role='presentation'
+//     />,
+//     < Image
+//       key={posts[4].id}
+//       src={posts[4].media_url}
+//       onDragStart={handleDragStart}
+//       className='item'
+//       role='presentation'
+//     />,
+//   ];
+
+//   return (
+//     <div className={`h-[auto] rounded-md overflow-hidden ${className}`}>
+//       <AliceCarousel
+//         autoPlay
+//         mouseTracking
+//         autoPlayInterval={2000}
+//         responsive={responsive}
+//         items={items}
+
+//         // // controlsStrategy='alternate'
+//       />
+//     </div>
+//   );
+// };
