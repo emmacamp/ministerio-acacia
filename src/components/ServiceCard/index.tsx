6367d1780a58d97941d61995763949e28c91b0c6
Emmanuel Popa <emmanuelaaronpopa@gmail.com>
2024-09-19 19:09:14 -0400

    adding images

--- src/components/ServiceCard/index.tsx
+++ src/components/ServiceCard/index.tsx
@@ -10,20 +10,24 @@ export const ServiceCard = ({ slug, title, description, image }: Props) => {
   const maxLength = 144;
   const shortenedDescription =
     description.length > maxLength ? description.substring(0, maxLength) + '...' : description;
+
   return (
     <Link
       href={`/servicios/${slug}`}
       data-aos='fade-up'
-      className={` flex flex-col items-center w-[32%] md:w-full`}
+      className='flex flex-col items-center w-[32%] md:w-full'
     >
       <div className='relative h-[462px] content-center self-stretch rounded-[20px]'>
+        {/* Imagen con filtro de brillo */}
         <Img
           src={image}
           width={388}
           height={462}
           alt={title}
-          className='mx-auto h-[462px] w-full flex-1 rounded-[20px] object-cover opacity-80'
+          className='mx-auto h-[462px] w-full flex-1 rounded-[20px] object-cover filter brightness-[0.4]'
         />
+
+        {/* Contenido del texto */}
         <div className='absolute bottom-[31%] left-0 right-0 m-auto flex flex-1 flex-col items-center gap-2 px-8 sm:px-5'>
           <Heading size='heading3' as='h1' className='!font-dmsans !text-white-a700 text-center'>
             {title}
@@ -31,7 +35,7 @@ export const ServiceCard = ({ slug, title, description, image }: Props) => {
           <Text
             size='texts'
             as='p'
-            className='self-stretch text-center !font-dmsans leading-[30px] !text-white-a700 '
+            className='self-stretch text-center !font-dmsans leading-[30px] !text-white-a700'
           >
             {shortenedDescription}
           </Text>
