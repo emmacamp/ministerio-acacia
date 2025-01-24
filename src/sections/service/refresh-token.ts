b095518ad9e8e1d2cc85b88abd624aa7838d1ba2
Emmanuel Popa <emmanuelaaronpopa@gmail.com>
2025-01-23 21:14:39 -0400

    refact

--- src/sections/service/refresh-token.ts
+++ src/sections/service/refresh-token.ts
@@ -1,12 +1,25 @@
+import { AxiosError } from 'axios';
+
 export const fetchInstagramToken = async () => {
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/refreshInstagramToken`
     );
-    const data = response.json();
-    console.log('Updated Token:', data);
-    return data;
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`);
+    }
+
+    const contentType = response.headers.get('content-type');
+    if (!contentType || !contentType.includes('application/json')) {
+      throw new Error('Response is not JSON');
+    }
+
+    console.log('Updated Token:', response);
+
+    return response.json();
   } catch (error) {
-    console.error('Error fetching Instagram token:', error);
+    const axios_error = error as AxiosError;
+    console.error('Fetch error:', axios_error?.message);
+    throw error;
   }
 };
